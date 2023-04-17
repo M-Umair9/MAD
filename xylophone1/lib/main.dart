e1631e0367ae620197446edb038a6a17d91c47e3
M-Umair9 <ch.umair111353@gmail.com>
2023-04-17 10:02:58 +0500

    Xylophone_Flutter_Packages

--- xylophone1/lib/main.dart
+++ xylophone1/lib/main.dart
@@ -1,24 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("xylophone"),
-          ),
-        )
-    )
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Xylophone'),
+        ),
+        body: Xylo(),
+      ),
+    );
   }
 }
-
-
 
 class Xylo extends StatefulWidget {
   const Xylo({Key? key}) : super(key: key);
@@ -28,8 +29,42 @@ class Xylo extends StatefulWidget {
 }
 
 class _XyloState extends State<Xylo> {
+  void PlaySound(int num){ AssetsAudioPlayer.newPlayer().open(
+    Audio("assets/assets_note$num.wav"),
+  );
+  }
+  CreateNewButton(int sound, Color color)
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          color: Colors.grey,
+          child: TextButton(
+              onPressed: () {
+                PlaySound(1);
+              },
+              child: Text("Button 1")),
+        ),
+        Container(
+          color: Colors.blueGrey,
+          child: TextButton(
+              onPressed: () {
+                PlaySound(2);
+              },
+              child: Text("Button 1")),
+        ),
+        Container(
+          color: Colors.black54,
+          child: TextButton(
+              onPressed: () {
+                PlaySound(3);
+              },
+              child: Text("Button 1")),
+        )
+      ],
+    );
   }
 }
